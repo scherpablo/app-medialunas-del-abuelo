258142e7c2dd2d951fb79e5c7902c500b405cd5c
scherpablo <scherpablo@gmail.com>
2024-06-28 23:58:04 -0300

    first commit

---
+++ prisma/data/categories.ts
@@ -0,0 +1,26 @@
+export const categories = [
+    {
+        slug: "cafes",
+        name: "Cafés"
+      },
+      {
+        slug: "hamburguesas",
+        name: "Hamburguesas"
+      },
+      {
+        slug: "pizzas",
+        name: "Pizzas"
+      },
+      {
+        slug: "donas",
+        name: "Donas"
+      },
+      {
+        slug: "pasteles",
+        name: "Pasteles"
+      },
+      {
+        slug: "galletas",
+        name: "Galletas"
+      }
+]
