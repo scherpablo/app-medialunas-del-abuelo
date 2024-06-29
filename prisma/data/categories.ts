1e09e3fa954975d3433f1654b202a90f14dab1ca
scherpablo <scherpablo@gmail.com>
2024-06-29 00:16:37 -0300

    update project half moons

--- prisma/data/categories.ts
+++ prisma/data/categories.ts
@@ -1,26 +1,33 @@
+// export const categories = [
+//       {
+//         slug: "cafes",
+//         name: "Cafés"
+//       },
+//       {
+//         slug: "hamburguesas",
+//         name: "Hamburguesas"
+//       },
+//       {
+//         slug: "pizzas",
+//         name: "Pizzas"
+//       },
+//       {
+//         slug: "donas",
+//         name: "Donas"
+//       },
+//       {
+//         slug: "pasteles",
+//         name: "Pasteles"
+//       },
+//       {
+//         slug: "galletas",
+//         name: "Galletas"
+//       }
+// ]
+
 export const categories = [
-    {
+      {
         slug: "cafes",
         name: "Cafés"
       },
-      {
-        slug: "hamburguesas",
-        name: "Hamburguesas"
-      },
-      {
-        slug: "pizzas",
-        name: "Pizzas"
-      },
-      {
-        slug: "donas",
-        name: "Donas"
-      },
-      {
-        slug: "pasteles",
-        name: "Pasteles"
-      },
-      {
-        slug: "galletas",
-        name: "Galletas"
-      }
 ]
