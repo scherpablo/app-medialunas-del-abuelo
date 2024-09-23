c65a27295eea32192d77aedaa17a78c39b22b4ee
Pablo Scherpa <desarrollador@pabloscherpa.com.ar>
2024-09-23 14:25:56 -0300

    update db

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -1,3 +1,524 @@
 export const products = [
-  
+  {
+    name: "Ades Naranja",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720879029/MedialunasDelAbuelo/productos/ades_naranja_200ml-100_wpk0da.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Benedicto S/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720877957/MedialunasDelAbuelo/productos/benedicto_sin_gas_b4wdbu.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Benedicto C/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720877957/MedialunasDelAbuelo/productos/benedicto_con_gas_e8kezt.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Bonaqua S/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720877283/MedialunasDelAbuelo/productos/bonaqua_sin_gas-100_vyrzia.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Bonaqua C/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720877443/MedialunasDelAbuelo/productos/bonaqua_con_gas-100_r5anv2.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Kin S/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720877590/MedialunasDelAbuelo/productos/kin_sin_gas-100_iegjum.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Kin C/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720877689/MedialunasDelAbuelo/productos/kin_con_gas-100_nsmifn.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Agua Smart S/Gas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1720878609/MedialunasDelAbuelo/productos/smart_sin_gas-100_bycvmp.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Baggio Manzana 125ml",
+    price: 1180,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721161742/MedialunasDelAbuelo/productos/baggio-manzana-125ml-100_vbpq7x.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Baggio Multifruta 125ml",
+    price: 1280,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721161742/MedialunasDelAbuelo/productos/baggio-multifruta-125ml-100_ne2xel.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Baggio Naranja 125ml",
+    price: 1380,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721161742/MedialunasDelAbuelo/productos/baggio-naranja-125ml-100_e4wiwl.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Cepita Botella 300ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721162538/MedialunasDelAbuelo/productos/cepita-naranja-botella300ml-100_xcecgi.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Cepita Manzana",
+    price: 1180,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721162001/MedialunasDelAbuelo/productos/cepita-manzana-caja-100_j1obof.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Cepita Multifruta",
+    price: 1280,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721162001/MedialunasDelAbuelo/productos/cepita-multifruta-caja-100_qehu3c.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Cepita Naranja",
+    price: 1380,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721162000/MedialunasDelAbuelo/productos/cepita-naranja-caja-100_rzbxzu.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Coca Lata 354ml",
+    price: 1000,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721162000/MedialunasDelAbuelo/productos/coca-lata-354ml-100_efatct.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Coca Lata S/Azúcar 354ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721164563/MedialunasDelAbuelo/productos/coca-sin-azucar-lata354ml-100_h4xv44.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Coca Original Vidrio 350ml",
+    price: 1200,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163043/MedialunasDelAbuelo/productos/coca-original-botella350ml-100_djofze.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Coca S/Azúcar Vidrio 350ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163044/MedialunasDelAbuelo/productos/coca-sin-azucar-botella350ml-100_nh18nt.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Estancia Lima Limón",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163881/MedialunasDelAbuelo/productos/estancia-lima-lim%C3%B3n-100_iufsvl.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Estancia Naranja",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163881/MedialunasDelAbuelo/productos/estancia-naranja-100_mxywzj.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Estancia Pomelo",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163882/MedialunasDelAbuelo/productos/estancia-pomelo-100_ayusre.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Estancia Naranja Durazno",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163881/MedialunasDelAbuelo/productos/estancia-naranaj-durazno-100_mfh8zm.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Estancia Frutilla Naranja",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163882/MedialunasDelAbuelo/productos/estancia-frutilla-naranja-100_kyf8w0.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Estancia Maracuyá Naranja",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721163882/MedialunasDelAbuelo/productos/estancia-maracuy%C3%A1-naranja-100_fdt56x.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Fanta Lata 354ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721165103/MedialunasDelAbuelo/productos/fanta-lata354ml-100_jzctvh.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Fanta Vidrio 350ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721165103/MedialunasDelAbuelo/productos/fanta-vidrio350ml-100_g0uqgp.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Sprite Lata 354ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721165103/MedialunasDelAbuelo/productos/sprite-lata354ml-100_iyk9ba.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Tónica Schweppes Lata 354ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721166119/MedialunasDelAbuelo/productos/schweppes-t%C3%B3nica-lata354ml-100_v5qx7f.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Pomelo Schweppes Lata 354ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721166119/MedialunasDelAbuelo/productos/schweppes-pomelo-lata354ml-100_xebenb.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Tónica Schweppes Vidrio 300ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721166119/MedialunasDelAbuelo/productos/schweppes-t%C3%B3nica-vidrio300ml-100_j8sans.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Pomelo Schweppes 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721166119/MedialunasDelAbuelo/productos/schweppes-pomelo-botella500ml-100_fpmv1i.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Monster Lata 473ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167500/MedialunasDelAbuelo/productos/monster-lata473ml-100_jzk3h2.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Limonada 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167498/MedialunasDelAbuelo/productos/aquarius-liomonada500ml-100_gxelut.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Manzana 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167498/MedialunasDelAbuelo/productos/aquarius-manzana500ml-100_ydtsne.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Naranja 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167498/MedialunasDelAbuelo/productos/aquarius-naranja500ml-100_ctmznw.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Pera 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167498/MedialunasDelAbuelo/productos/aquarius-pera500ml-100_ncbh30.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Pomelo 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167498/MedialunasDelAbuelo/productos/aquarius-pomelo500ml-100_gvj5tf.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Pomelo Rosado 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167499/MedialunasDelAbuelo/productos/aquarius-rosado500ml-100_nob3ci.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Aquarius Uva 500ml",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721167499/MedialunasDelAbuelo/productos/aquarius-uva500ml-100_dshlmn.jpg",
+    categoryId: 1
+  },
+  {
+    name: "Café Lágrima",
+    price: 3200,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861905/MedialunasDelAbuelo/productos/cafe-lagrima-100_qgnu1h.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Café Irlandés",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861905/MedialunasDelAbuelo/productos/cafe-irlandes-100_llkuj3.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Capuccino C/Crema",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861907/MedialunasDelAbuelo/productos/capuccino-con-crema-100_i2mvhn.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Capuccino Italiano",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721862593/MedialunasDelAbuelo/productos/capuccino-italiano-100_q0mgji.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Capuccino Amaretti",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861905/MedialunasDelAbuelo/productos/capuccino-amaretti-100_yoytha.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Capuccino Avellana",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861906/MedialunasDelAbuelo/productos/capuccino-avellana-100_v4sfwk.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Capuccino Caramelo",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861907/MedialunasDelAbuelo/productos/capuccino-dulce-de-leche-100_r5jqzl.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Capuccino Vainilla",
+    price: 3490,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861907/MedialunasDelAbuelo/productos/capuccino-vainilla-100_r2wwjl.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Chocolatada en Vaso",
+    price: 2920,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861907/MedialunasDelAbuelo/productos/chocolatada-en-vaso-100_uz9vrh.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Café Cortado Doble",
+    price: 2920,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861905/MedialunasDelAbuelo/productos/cafe-cortado-doble-100_ugjg3h.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Jarrito Cortado",
+    price: 2920,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861908/MedialunasDelAbuelo/productos/jarrito-cortado-100_vjqyl7.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Jarrito Lágrima",
+    price: 2920,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861908/MedialunasDelAbuelo/productos/jarrito-lagrima-100_bxna1x.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Pocillo Café",
+    price: 2340,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861921/MedialunasDelAbuelo/productos/pocillo-cafe-100_olerai.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Pocillo Cortado",
+    price: 1500,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861921/MedialunasDelAbuelo/productos/pocillo-cortado-100_v8n5vo.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Pocillo Lágrima",
+    price: 1500,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861921/MedialunasDelAbuelo/productos/pocillo-lagrima-100_iymqhz.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Submarino",
+    price: 400,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1721861922/MedialunasDelAbuelo/productos/submarino-100_dhnv7h.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Té",
+    price: 400,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726171934/MedialunasDelAbuelo/productos/taza-de-te-100_ycw43v.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Té Saborizado",
+    price: 400,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726171934/MedialunasDelAbuelo/productos/taza-de-te-100_ycw43v.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Mate Cocido",
+    price: 400,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726171934/MedialunasDelAbuelo/productos/taza-mate-cocido-100_w4udcu.jpg",
+    categoryId: 2
+  },
+  {
+    name: "Café C/Leche + 2 medialunas",
+    price: 1900,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726173573/MedialunasDelAbuelo/productos/cafe-con-leche-2-medialunas-100_svxyii.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + 3 medialunas",
+    price: 1480,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726173574/MedialunasDelAbuelo/productos/cafe-con-leche-3-medialunas-100_jh2abv.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + 2 facturas",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726182428/MedialunasDelAbuelo/productos/cafe-con-leche-2-facturas-100_fdupby.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + 3 facturas",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726173574/MedialunasDelAbuelo/productos/cafe-con-leche-3-facturas-100_qaqrvd.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + 1/2 tostado",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726173574/MedialunasDelAbuelo/productos/cafe-con-leche-1-tostado-100_arwbse.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + 1/2 tostado",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726174919/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-tostado-100_b8muii.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + 2 medialunas JyQ",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726174918/MedialunasDelAbuelo/productos/cafe-con-leche-2-medialunas-jyq-100_fmzgan.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + 2 medialunas JyQ",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726174919/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-2-medialunas-jyq_1-100_pztesq.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + arabe JyQ",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726174918/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-arabe-jyq-100_jgw4ce.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + 2 fosforitos JyQ",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726175884/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-fosforitos-100_fbpzby.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + 2 paninis JyQ",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726175884/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-paninis-100_iexhxd.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + porción torta",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726175883/MedialunasDelAbuelo/productos/cafe-con-leche-porcion-de-torta-100_czxndh.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + porción torta",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726175883/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-porcion-de-torta-100_vikgvq.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + yogurt + porción torta",
+    price: 1830,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726182428/MedialunasDelAbuelo/productos/cafe-con-leche-yogurt-porcion-torta-100_v7nwla.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + jugo + tostado pan frances",
+    price: 9999,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726182429/MedialunasDelAbuelo/productos/cafe-con-leche-jugo-pan-frances-100_af8knl.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + yogurt + tostado pan frances",
+    price: 9999,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726182428/MedialunasDelAbuelo/productos/cafe-con-leche-yogurt-pan-frances-100_qhsclr.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Capuccino + 3 medialunas",
+    price: 1900,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726183400/MedialunasDelAbuelo/productos/capuccino-3-medialunas-100_gbhkz3.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Capuccino + 1/2 tostado",
+    price: 1900,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726183400/MedialunasDelAbuelo/productos/capuccino-tostado-100_s271ed.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Capuccino + arabe JyQ",
+    price: 1900,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726183400/MedialunasDelAbuelo/productos/capuccino-pan-arabe-100_pbg0pz.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + Budín Artesanal",
+    price: 2700,
+    image: "taza_cafe",
+    categoryId: 3
+  },
+  {
+    name: "Café C/Leche + 1 Alfajor Artesanal",
+    price: 3600,
+    image: "taza_cafe",
+    categoryId: 3
+  },
+  {
+    name: "Jarrito + 1 factura",
+    price: 2920,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726680524/MedialunasDelAbuelo/productos/jarrito-1-factura-100_glomqb.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Jarrito + 1 medialuna",
+    price: 2920,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726680524/MedialunasDelAbuelo/productos/jarrito-1-medialuna-100_dqfyu7.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Pocillo + 1 factura",
+    price: 1850,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726680524/MedialunasDelAbuelo/productos/pocillo-1-factura-100_glly9e.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Pocillo + 1 medialuna",
+    price: 3800,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726680524/MedialunasDelAbuelo/productos/pocillo-1-medialuna-100_pkwvxd.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Taza + 1 factura",
+    price: 3800,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726680524/MedialunasDelAbuelo/productos/taza-1-factura-100_vbv80a.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Taza + 1 medialuna",
+    price: 3800,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726680524/MedialunasDelAbuelo/productos/taza-1-medialuna-100_vuldzn.jpg",
+    categoryId: 3
+  },
+  {
+    name: "Submarino + 3 medialunas",
+    price: 400,
+    image: "https://res.cloudinary.com/dneo9bcez/image/upload/v1726681323/MedialunasDelAbuelo/productos/submarino-3-medialunas-100_hzh4wx.jpg",
+    categoryId: 3
+  },
 ]
